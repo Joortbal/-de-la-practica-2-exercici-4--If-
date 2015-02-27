fbc62e153612f1b52d7e8c4fe0fa26e7f5d02ff3
Joortbal <jordi_bct@hotmail.com>
2015-02-27 20:44:41 +0100

    EXERCICIS
    exercici

--- sketch_feb27a/sketch_feb27a.ino
+++ sketch_feb27a/sketch_feb27a.ino
@@ -1,19 +1,41 @@
-int tempAigua = 99;
+int nivellCO2 = 50;
 
 void setup()              // run once, when the sketch starts
 {
   Serial.begin(9600);     // set up Serial library at 9600 bps
 
-  if ( tempAigua >= 100)
+  if ( nivellCO2 <3.5)
   {
-    Serial.print("Aigua bullint!");
+    Serial.print("A");
   } 
 
-  else
+ 
+ else if( nivellCO2 <6.5)
   {
-    Serial.print("Aigua encara no bull!");
+    Serial.print("B");
   } 
-}
+  else if( nivellCO2 <11.1)
+  {
+    Serial.print("c");
+  } 
+  else if( nivellCO2 <17.7)
+  {
+    Serial.print("D");
+  } 
+  else if( nivellCO2 <38.2)
+  {
+    Serial.print("E");
+  } 
+  else if( nivellCO2 <43.2)
+  {
+    Serial.print("F");
+  } 
+ 
+else
+  {
+    Serial.print("G");
+  }
+  }
 
 void loop()   // we need this to be here even though its empty
 {
